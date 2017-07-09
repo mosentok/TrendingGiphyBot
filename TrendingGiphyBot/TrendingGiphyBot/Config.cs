011ab80e03db94a684ad0ed51298bfb87d55ebfa
Kirk Horton <mosentok@gmail.com>
2017-07-09 10:22:52 -0500

    Welp fluent scheduler turns out suffers from drifiting timers and I couldn't figure out an inherent way to make it not so back to timers.

--- TrendingGiphyBot/TrendingGiphyBot/Config.cs
+++ TrendingGiphyBot/TrendingGiphyBot/Config.cs
@@ -4,5 +4,8 @@
     {
         public string DiscordToken { get; set; }
         public string GiphyToken { get; set; }
+        public int Hour { get; set; }
+        public int Minutes { get; set; }
+        public int RunEveryXSeconds { get; set; } = 5;
     }
 }
