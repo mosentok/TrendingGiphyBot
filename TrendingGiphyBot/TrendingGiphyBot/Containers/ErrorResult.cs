bdf7dcb67689e2a56a8c1153d06ca75f2b25c071
mosentok <mosentok@gmail.com>
2017-07-11 00:30:30 -0500

    Now returns error as JSON.

---
+++ TrendingGiphyBot/TrendingGiphyBot/Containers/ErrorResult.cs
@@ -0,0 +1,20 @@
+﻿using Discord.Commands;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace TrendingGiphyBot.Containers
+{
+    public class ErrorResult
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public CommandError? Error { get; set; }
+        public string ErrorReason { get; set; }
+        public bool IsSuccess { get; set; }
+        public ErrorResult(IResult result)
+        {
+            Error = result.Error;
+            ErrorReason = result.ErrorReason;
+            IsSuccess = IsSuccess;
+        }
+    }
+}
