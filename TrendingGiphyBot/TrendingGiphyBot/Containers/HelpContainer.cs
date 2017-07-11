baafef76dda0a5aee399ec01af66e7ea55e3e68f
Kirk Horton <mosentok@gmail.com>
2017-07-10 23:27:32 -0500

    Simplified help container.

--- TrendingGiphyBot/TrendingGiphyBot/Containers/HelpContainer.cs
+++ TrendingGiphyBot/TrendingGiphyBot/Containers/HelpContainer.cs
@@ -5,16 +5,10 @@ namespace TrendingGiphyBot.Containers
 {
     class HelpContainer
     {
-        public string Name { get; set; }
-        public string Summary { get; set; }
-        public IEnumerable<string> Aliases { get; set; }
         [JsonProperty("Commands")]
         public IEnumerable<MethodContainer> Methods { get; set; }
-        public HelpContainer(string name, string summary, IEnumerable<string> aliases, IEnumerable<MethodContainer> methods)
+        public HelpContainer(IEnumerable<MethodContainer> methods)
         {
-            Name = name;
-            Summary = summary;
-            Aliases = aliases;
             Methods = methods;
         }
     }
