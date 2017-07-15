6f4f53e7a4837b7db24af0f2dd3c878e37b1753e
Kirk Horton <mosentok@gmail.com>
2017-07-15 13:47:27 -0500

    Moved job config state logic into Job.

--- TrendingGiphyBot/TrendingGiphyBot/Helpers/ModuleBaseHelper.cs
+++ TrendingGiphyBot/TrendingGiphyBot/Helpers/ModuleBaseHelper.cs
@@ -1,22 +1,18 @@
 ﻿using Discord;
 using Discord.Commands;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using TrendingGiphyBot.Attributes;
 using TrendingGiphyBot.Enums;
-using TrendingGiphyBot.Exceptions;
+using TrendingGiphyBot.Jobs;
 
 namespace TrendingGiphyBot.Helpers
 {
-    //TODO all the job state logic could probably be cleaner
     static class ModuleBaseHelper
     {
-        static readonly List<int> _ValidMinutesSeconds = new List<int> { 1, 5, 10, 15, 20, 30 };
-        static readonly List<int> _ValidHours = new List<int> { 1, 2, 3, 4, 6, 8, 12, 24 };
-        internal static string ValidMinutesSecondsString => string.Join(", ", _ValidMinutesSeconds.Select(s => s.ToString()));
-        internal static string ValidHoursString => string.Join(", ", _ValidHours.Select(s => s.ToString()));
+        internal static string ValidMinutesSecondsString => string.Join(", ", Job.ValidMinutesSeconds.Select(s => s.ToString()));
+        internal static string ValidHoursString => string.Join(", ", Job.ValidHours.Select(s => s.ToString()));
         internal static string InvalidConfig(Time time, string validValues) =>
             $"When {nameof(Time)} is {time}, interval must be {validValues}.";
         internal static string InvalidConfigRange(int minimumMinutes, int maximumMinutes) =>
@@ -68,55 +64,5 @@ namespace TrendingGiphyBot.Helpers
             return $"{method.Name}({parameters})";
         }
         static string RemoveNamespace(string parameterType) => parameterType.Split(new[] { '.' }).Last();
-        internal static JobConfigState DetermineJobConfigState(int interval, Time time, int minimumSeconds, int maximumSeconds)
-        {
-            var configgedSeconds = DetermineConfiggedSeconds(interval, time);
-            if (configgedSeconds >= minimumSeconds)
-            {
-                if (configgedSeconds <= maximumSeconds)
-                    switch (time)
-                    {
-                        case Time.Hour:
-                        case Time.Hours:
-                            if (_ValidHours.Contains(interval))
-                                return JobConfigState.Valid;
-                            return JobConfigState.InvalidHours;
-                        case Time.Minute:
-                        case Time.Minutes:
-                            return IsValid(interval, JobConfigState.InvalidMinutes);
-                        case Time.Second:
-                        case Time.Seconds:
-                            return IsValid(interval, JobConfigState.InvalidSeconds);
-                        default:
-                            return JobConfigState.InvalidTime;
-                    }
-                return JobConfigState.IntervallTooBig;
-            }
-            return JobConfigState.IntervalTooSmall;
-        }
-        static JobConfigState IsValid(int interval, JobConfigState invalidState)
-        {
-            var isValidMinuteSecond = _ValidMinutesSeconds.Contains(interval);
-            if (isValidMinuteSecond)
-                return JobConfigState.Valid;
-            return invalidState;
-        }
-        internal static double DetermineConfiggedSeconds(int interval, Time time)
-        {
-            switch (time)
-            {
-                case Time.Hour:
-                case Time.Hours:
-                    return TimeSpan.FromHours(interval).TotalSeconds;
-                case Time.Minute:
-                case Time.Minutes:
-                    return TimeSpan.FromMinutes(interval).TotalSeconds;
-                case Time.Second:
-                case Time.Seconds:
-                    return TimeSpan.FromSeconds(interval).TotalSeconds;
-                default:
-                    throw new InvalidTimeException(time);
-            }
-        }
     }
 }
