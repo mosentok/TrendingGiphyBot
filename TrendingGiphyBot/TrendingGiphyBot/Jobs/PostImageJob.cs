75a40da07315ff7e7cadea1c01e576901e792d27
mosentok <mosentok@gmail.com>
2017-07-23 23:14:33 -0500

    Added min/max quiet hour colums to job config. Added logic to inhibit posting during quiet hours.

--- TrendingGiphyBot/TrendingGiphyBot/Jobs/PostImageJob.cs
+++ TrendingGiphyBot/TrendingGiphyBot/Jobs/PostImageJob.cs
@@ -19,21 +19,22 @@ namespace TrendingGiphyBot.Jobs
             if (await GlobalConfig.UrlCacheDal.Any())
             {
                 var latestUrl = await GlobalConfig.UrlCacheDal.GetLatestUrl();
-                if (!string.IsNullOrEmpty(latestUrl))
-                {
-                    var channelsNotPostedToYet = JobConfigs.Where(s => UrlHasNotBeenPostedToChannel(Convert.ToUInt64(s.ChannelId), latestUrl)).ToList();
-                    await PostNewUrls(latestUrl, channelsNotPostedToYet);
-                    var channelsStillNotPostedToYet = JobConfigs.Except(channelsNotPostedToYet).ToList();
-                    var channelsWithRandomStringOn = channelsStillNotPostedToYet.Where(s => s.RandomIsOn && !string.IsNullOrWhiteSpace(s.RandomSearchString));
-                    await PostRandomsWithSearchString(channelsWithRandomStringOn);
-                    var channelsLeftover = JobConfigs
-                        .Except(channelsNotPostedToYet)
-                        .Except(channelsStillNotPostedToYet)
-                        .Where(s => s.RandomIsOn);
-                    var giphyRandomResult = await GlobalConfig.GiphyClient.RandomGif(new RandomParameter());
-                    var randomUrl = giphyRandomResult.Data.Url;
-                    await PostChannelsLeftover(channelsLeftover, randomUrl);
-                }
+                var nowHour = DateTime.Now.Hour;
+                var channelsNotPostedToYet = JobConfigs
+                    .Where(s => UrlHasNotBeenPostedToChannel(Convert.ToUInt64(s.ChannelId), latestUrl)
+                        && (!s.MinQuietHour.HasValue || nowHour < s.MinQuietHour)
+                        && (!s.MaxQuietHour.HasValue || nowHour > s.MaxQuietHour)).ToList();
+                await PostNewUrls(latestUrl, channelsNotPostedToYet);
+                var channelsStillNotPostedToYet = JobConfigs.Except(channelsNotPostedToYet).ToList();
+                var channelsWithRandomStringOn = channelsStillNotPostedToYet.Where(s => s.RandomIsOn && !string.IsNullOrWhiteSpace(s.RandomSearchString));
+                await PostRandomsWithSearchString(channelsWithRandomStringOn);
+                var channelsLeftover = JobConfigs
+                    .Except(channelsNotPostedToYet)
+                    .Except(channelsStillNotPostedToYet)
+                    .Where(s => s.RandomIsOn);
+                var giphyRandomResult = await GlobalConfig.GiphyClient.RandomGif(new RandomParameter());
+                var randomUrl = giphyRandomResult.Data.Url;
+                await PostChannelsLeftover(channelsLeftover, randomUrl);
             }
         }
         async Task PostNewUrls(string latestUrl, List<JobConfig> channelsNotPostedToYet)
