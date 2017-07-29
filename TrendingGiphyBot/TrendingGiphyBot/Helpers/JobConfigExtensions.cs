8384dc5c33befad411b8bef9a7b7de6fe36c0d2e
Kirk Horton <mosentok@gmail.com>
2017-07-29 11:20:25 -0500

    Should now handle when min or max are 0.

---
+++ TrendingGiphyBot/TrendingGiphyBot/Helpers/JobConfigExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TrendingGiphyBot.Dals;
+
+namespace TrendingGiphyBot.Helpers
+{
+    internal static class JobConfigExtensions
+    {
+        internal static bool IsInQuietHours(this JobConfig jobConfig, int nowHour)
+        {
+            if (jobConfig.MinQuietHour.HasValue && jobConfig.MaxQuietHour.HasValue)
+            {
+                IEnumerable<int> quietHours;
+                if (jobConfig.MinQuietHour.Value == jobConfig.MaxQuietHour.Value)
+                    quietHours = new List<int> { jobConfig.MinQuietHour.Value };
+                if (jobConfig.MinQuietHour.Value < jobConfig.MaxQuietHour.Value)
+                    quietHours = Enumerable.Range(jobConfig.MinQuietHour.Value, jobConfig.MaxQuietHour.Value - jobConfig.MinQuietHour.Value + 1);
+                else
+                {
+                    var minHours = Enumerable.Range(jobConfig.MinQuietHour.Value, 24 - jobConfig.MinQuietHour.Value);
+                    var maxHours = Enumerable.Range(0, jobConfig.MaxQuietHour.Value + 1);
+                    quietHours = minHours.Concat(maxHours);
+                }
+                return quietHours.Contains(nowHour);
+            }
+            return false;
+        }
+    }
+}
