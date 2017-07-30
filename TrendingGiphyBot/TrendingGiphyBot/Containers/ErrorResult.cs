502f95d6c7dda952428f44dea80772e382150c21
Kirk Horton <mosentok@gmail.com>
2017-07-30 10:49:08 -0500

    Wrapped main program in using.

--- TrendingGiphyBot/TrendingGiphyBot/Containers/ErrorResult.cs
+++ TrendingGiphyBot/TrendingGiphyBot/Containers/ErrorResult.cs
@@ -2,13 +2,13 @@
 
 namespace TrendingGiphyBot.Containers
 {
-    public class ErrorResult
+    class ErrorResult
     {
-        public CommandError? Error { get; set; }
-        public string ErrorReason { get; set; }
-        public bool IsSuccess { get; set; }
-        public ErrorResult(IResult result) : this(result.Error, result.ErrorReason, result.IsSuccess) { }
-        public ErrorResult(CommandError? error, string errorReason, bool isSuccess)
+        internal CommandError? Error { get; set; }
+        internal string ErrorReason { get; set; }
+        internal bool IsSuccess { get; set; }
+        internal ErrorResult(IResult result) : this(result.Error, result.ErrorReason, result.IsSuccess) { }
+        internal ErrorResult(CommandError? error, string errorReason, bool isSuccess)
         {
             Error = error;
             ErrorReason = errorReason;
