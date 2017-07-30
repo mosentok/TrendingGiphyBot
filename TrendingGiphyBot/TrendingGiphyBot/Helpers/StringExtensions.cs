7ea62b8d96523ebbc8421c6f1bde450137b98979
mosentok <mosentok@gmail.com>
2017-07-30 14:39:24 -0500

    Will now respond only to recognized commands.

---
+++ TrendingGiphyBot/TrendingGiphyBot/Helpers/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+namespace TrendingGiphyBot.Helpers
+{
+    internal static class StringExtensions
+    {
+        internal static bool ContainsIgnoreCase(this string paragraph, string word) =>
+            CultureInfo.InvariantCulture.CompareInfo.IndexOf(paragraph, word, CompareOptions.IgnoreCase) >= 0;
+    }
+}
