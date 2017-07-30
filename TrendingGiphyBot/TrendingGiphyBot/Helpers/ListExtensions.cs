0593f937af4bc46b7215518a4aafb6ddd3a0eb8f
mosentok <mosentok@gmail.com>
2017-07-30 11:01:50 -0500

    Refactoring.

---
+++ TrendingGiphyBot/TrendingGiphyBot/Helpers/ListExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace TrendingGiphyBot.Helpers
+{
+    static class ListExtensions
+    {
+        internal static string Join(this List<int> ints, string separator) => string.Join(separator, ints);
+    }
+}
