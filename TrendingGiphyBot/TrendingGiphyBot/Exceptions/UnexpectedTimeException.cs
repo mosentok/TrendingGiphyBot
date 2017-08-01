6e8e5a3513094901f2803df74765038afe3b9be1
mosentok <mosentok@gmail.com>
2017-07-31 22:08:19 -0500

    Added UnexpectedLogSeverityException.

---
+++ TrendingGiphyBot/TrendingGiphyBot/Exceptions/UnexpectedTimeException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Serialization;
+using TrendingGiphyBot.Enums;
+
+namespace TrendingGiphyBot.Exceptions
+{
+    [Serializable]
+    public class UnexpectedTimeException : Exception
+    {
+        public UnexpectedTimeException() { }
+        public UnexpectedTimeException(Time time) : this($"{time} is an unexpected {nameof(Time)}.") { }
+        public UnexpectedTimeException(string message) : base(message) { }
+        public UnexpectedTimeException(string message, Exception innerException) : base(message, innerException) { }
+        protected UnexpectedTimeException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
