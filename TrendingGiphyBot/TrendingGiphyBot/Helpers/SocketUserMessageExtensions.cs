5a7b5ec0de62d35165ca4a5d0442162a7ddee858
Kirk Horton <mosentok@gmail.com>
2017-08-06 13:22:01 -0500

    Added IsRecognizedModule extension for SocketUserMessage. Combined if statements.

---
+++ TrendingGiphyBot/TrendingGiphyBot/Helpers/SocketUserMessageExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Discord.WebSocket;
+
+namespace TrendingGiphyBot.Helpers
+{
+    static class SocketUserMessageExtensions
+    {
+        internal static bool IsRecognizedModule(this SocketUserMessage message, List<string> modules) =>
+            modules.Any(message.Content.ContainsIgnoreCase);
+    }
+}
