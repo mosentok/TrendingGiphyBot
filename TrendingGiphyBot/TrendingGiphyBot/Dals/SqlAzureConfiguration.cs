fd6273f79cb54b0b407d0c1acee5da7e2a2d323e
mosentok <mosentok@gmail.com>
2018-02-24 10:13:08 -0600

    Now sets db config to SqlAzureConfiguration to retry transient failures. Updated nuget packages.

---
+++ TrendingGiphyBot/TrendingGiphyBot/Dals/SqlAzureConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+
+namespace TrendingGiphyBot.Dals
+{
+    public class SqlAzureConfiguration : DbConfiguration
+    {
+        public SqlAzureConfiguration()
+        {
+            SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
+        }
+    }
+}
