fa89844c7db401356c859c7e60de10fea4d8528c
Kirk Horton <mosentok@gmail.com>
2018-06-13 00:10:53 -0500

    Revert "Histories are now batched in a list after successful gif posting then sent to the DB at the end."
    This reverts commit ad5c0dc47af6883267eb24ec5c1e1e06fe14e2ed.

--- TrendingGiphyBot/TrendingGiphyBot/Jobs/PostImageJob.cs
+++ TrendingGiphyBot/TrendingGiphyBot/Jobs/PostImageJob.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Threading.Tasks;
 using Discord.WebSocket;
 using TrendingGiphyBot.Dals;
@@ -23,16 +24,14 @@ namespace TrendingGiphyBot.Jobs
             if (currentValidMinutes.Any() && GlobalConfig.LatestUrlsOrdered.Any())
                 await Logger.SwallowAsync(async () =>
                 {
-                    var successes = new List<UrlHistory>();
                     var jobConfigs = await GlobalConfig.JobConfigDal.Get(currentValidMinutes);
                     var jobConfigsNotInQuietHours = jobConfigs.Where(s => !s.IsInQuietHours()).ToList();
-                    var jobConfigsJustPostedTo = await PostChannelsNotInQuietHours(jobConfigsNotInQuietHours, successes);
+                    var jobConfigsJustPostedTo = await PostChannelsNotInQuietHours(jobConfigsNotInQuietHours);
                     var remainingJobConfigs = jobConfigsNotInQuietHours.Except(jobConfigsJustPostedTo).Where(s => s.RandomIsOn).ToList();
                     var jobConfigsWithRandomStringOn = remainingJobConfigs.Where(s => !string.IsNullOrEmpty(s.RandomSearchString)).ToList();
                     var jobConfigsWithRandomStringOff = remainingJobConfigs.Except(jobConfigsWithRandomStringOn).ToList();
-                    await PostChannelsWithRandomStringOn(jobConfigsWithRandomStringOn, successes);
-                    await PostChannelsWithRandomStringOff(jobConfigsWithRandomStringOff, successes);
-                    await GlobalConfig.UrlHistoryDal.Insert(successes);
+                    await PostChannelsWithRandomStringOn(jobConfigsWithRandomStringOn);
+                    await PostChannelsWithRandomStringOff(jobConfigsWithRandomStringOff);
                 });
         }
         List<int> DetermineCurrentValidMinutes()
@@ -43,24 +42,24 @@ namespace TrendingGiphyBot.Jobs
                 totalMinutes = 24 * 60;
             return GlobalConfig.AllValidMinutes.Where(s => totalMinutes % s == 0).ToList();
         }
-        async Task<List<JobConfig>> PostChannelsNotInQuietHours(List<JobConfig> jobConfigsNotInQuietHours, List<UrlHistory> successes)
+        async Task<ConcurrentBag<JobConfig>> PostChannelsNotInQuietHours(List<JobConfig> jobConfigsNotInQuietHours)
         {
-            var channelsPostedTo = new List<JobConfig>();
+            var channelsPostedTo = new ConcurrentBag<JobConfig>();
             foreach (var jobConfig in jobConfigsNotInQuietHours)
                 if (DiscordClient.GetChannel(jobConfig.ChannelId.ToULong()) is SocketTextChannel socketTextChannel)
-                    await PostFirstNewGif(channelsPostedTo, jobConfig, socketTextChannel, successes);
+                    await PostFirstNewGif(channelsPostedTo, jobConfig, socketTextChannel);
             return channelsPostedTo;
         }
-        async Task PostFirstNewGif(List<JobConfig> channelsPostedTo, JobConfig jobConfig, SocketTextChannel socketTextChannel, List<UrlHistory> successes)
+        async Task PostFirstNewGif(ConcurrentBag<JobConfig> channelsPostedTo, JobConfig jobConfig, SocketTextChannel socketTextChannel)
         {
             var url = await GlobalConfig.LatestUrlsOrdered.FirstOrDefaultAsync(async s => !await GlobalConfig.UrlHistoryDal.Any(jobConfig.ChannelId, s));
             if (url != default)
             {
-                await PostGif(jobConfig.ChannelId, url, $"*Trending!* {url}", socketTextChannel, successes);
+                await PostGif(jobConfig.ChannelId, url, $"*Trending!* {url}", socketTextChannel);
                 channelsPostedTo.Add(jobConfig);
             }
         }
-        async Task PostChannelsWithRandomStringOn(List<JobConfig> jobConfigsWithRandomStringOn, List<UrlHistory> successes)
+        async Task PostChannelsWithRandomStringOn(List<JobConfig> jobConfigsWithRandomStringOn)
         {
             foreach (var jobConfig in jobConfigsWithRandomStringOn)
                 await Logger.SwallowAsync(async () =>
@@ -69,7 +68,7 @@ namespace TrendingGiphyBot.Jobs
                     {
                         var randomParameter = new RandomParameter { Rating = GlobalConfig.Ratings, Tag = jobConfig.RandomSearchString };
                         var giphyRandomResult = await GlobalConfig.GiphyClient.RandomGif(randomParameter);
-                        await PostRandomGif(jobConfig, giphyRandomResult.Data.Url, successes);
+                        await PostRandomGif(jobConfig, giphyRandomResult.Data.Url);
                     }
                     catch (JsonSerializationException jsEx)
                     {
@@ -81,29 +80,29 @@ namespace TrendingGiphyBot.Jobs
                     }
                 });
         }
-        async Task PostChannelsWithRandomStringOff(List<JobConfig> jobConfigsWithRandomStringOff, List<UrlHistory> successes)
+        async Task PostChannelsWithRandomStringOff(List<JobConfig> jobConfigsWithRandomStringOff)
         {
             if (jobConfigsWithRandomStringOff.Any())
                 await Logger.SwallowAsync(async () =>
                 {
                     var giphyRandomResult = await GlobalConfig.GiphyClient.RandomGif(new RandomParameter { Rating = GlobalConfig.Ratings });
                     foreach (var jobConfig in jobConfigsWithRandomStringOff)
-                        await PostRandomGif(jobConfig, giphyRandomResult.Data.Url, successes);
+                        await PostRandomGif(jobConfig, giphyRandomResult.Data.Url);
                 });
         }
-        async Task PostRandomGif(JobConfig jobConfig, string url, List<UrlHistory> successes)
+        async Task PostRandomGif(JobConfig jobConfig, string url)
         {
             if (!await GlobalConfig.UrlHistoryDal.Any(jobConfig.ChannelId, url)
                 && DiscordClient.GetChannel(jobConfig.ChannelId.ToULong()) is SocketTextChannel socketTextChannel)
-                await PostGif(jobConfig.ChannelId, url, url, socketTextChannel, successes);
+                await PostGif(jobConfig.ChannelId, url, url, socketTextChannel);
         }
-        async Task PostGif(decimal channelId, string url, string message, SocketTextChannel socketTextChannel, List<UrlHistory> successes)
+        async Task PostGif(decimal channelId, string url, string message, SocketTextChannel socketTextChannel)
         {
             try
             {
                 await socketTextChannel.SendMessageAsync(message);
                 var history = new UrlHistory { ChannelId = channelId, Url = url };
-                successes.Add(history);
+                await GlobalConfig.UrlHistoryDal.Insert(history);
             }
             catch (HttpException httpException) when (GlobalConfig.Config.HttpExceptionsToWarn.Contains(httpException.Message))
             {
