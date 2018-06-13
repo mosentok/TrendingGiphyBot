be6679fc22e83401d3a86a59c0628728846c0a68
Kirk Horton <mosentok@gmail.com>
2018-06-13 00:45:00 -0500

    Success are now inserted one at a time in a try catch. Still kept at the end of the workflow so that gifs get posted first.

--- TrendingGiphyBot/TrendingGiphyBot/Dals/UrlHistoryDal.cs
+++ TrendingGiphyBot/TrendingGiphyBot/Dals/UrlHistoryDal.cs
@@ -1,25 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
+using NLog;
 
 namespace TrendingGiphyBot.Dals
 {
     public class UrlHistoryDal : Dal
     {
+        static readonly ILogger _Logger = LogManager.GetCurrentClassLogger();
         internal UrlHistoryDal(string connectionString) : base(connectionString) { }
         internal async Task<bool> Any(decimal channelId, string url)
         {
             using (var entities = new TrendingGiphyBotEntities(ConnectionString))
                 return await entities.UrlHistories.AnyAsync(s => s.ChannelId == channelId && s.Url == url);
         }
-        internal async Task Insert(UrlHistory urlHistory)
+        internal async Task Insert(List<UrlHistory> histories)
         {
             using (var entities = new TrendingGiphyBotEntities(ConnectionString))
-            {
-                entities.UrlHistories.Add(urlHistory);
-                await entities.SaveChangesAsync();
-            }
+                foreach (var history in histories)
+                    await _Logger.SwallowAsync(async () =>
+                    {
+                        entities.UrlHistories.Add(history);
+                        await entities.SaveChangesAsync();
+                    });
         }
         internal async Task DeleteOlderThan(DateTime oldestDate)
         {
