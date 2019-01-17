e167430c83b75a1279a0e7c82255235b400351d1
Kirk Horton <mosentok@gmail.com>
2019-01-17 12:05:22 -0600

    Added PostJobConfigFunction to update or insert requests. Moved that db logic from bot to function. Added FunctionHelper.SetJobConfigAsync to call this function from the bot. The Get command now calls the Get function. The Every command now calls the Post function. The other commands still need to be updated.

---
+++ TrendingGiphyBot/TrendingGiphyBotFunctions/PostJobConfigFunction.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using TrendingGiphyBotFunctions.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace TrendingGiphyBotFunctions
+{
+    public static class PostJobConfigFunction
+    {
+        [FunctionName(nameof(PostJobConfigFunction))]
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "jobconfigs/{channelid:decimal}")] HttpRequest req, decimal channelId, ILogger log)
+        {
+            string content;
+            using (var reader = new StreamReader(req.Body))
+                content = await reader.ReadToEndAsync();
+            var container = JsonConvert.DeserializeObject<JobConfigContainer>(content);
+            var connectionString = Environment.GetEnvironmentVariable("ConnectionString");
+            JobConfigContainer result;
+            using (var context = new TrendingGiphyBotContext(connectionString))
+                result = await context.SetJobConfig(channelId, container);
+            return new OkObjectResult(result);
+        }
+    }
+}
