0fc10c9365fa93e23cfdfd9a822597dd101606b9
Kirk Horton <mosentok@gmail.com>
2019-01-19 15:33:03 -0600

    Wrapped disposable instances in using statements.

--- TrendingGiphyBot/TrendingGiphyBot/Extensions/HttpClientExtensions.cs
+++ TrendingGiphyBot/TrendingGiphyBot/Extensions/HttpClientExtensions.cs
@@ -8,30 +8,38 @@ namespace TrendingGiphyBot.Extensions
     {
         public static Task<HttpResponseMessage> GetWithHeaderAsync(this HttpClient httpClient, string requestUri, string headerName, string headerValue)
         {
-            var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
-            request.Headers.Add(headerName, headerValue);
-            return httpClient.SendAsync(request);
+            using (var request = new HttpRequestMessage(HttpMethod.Get, requestUri))
+            {
+                request.Headers.Add(headerName, headerValue);
+                return httpClient.SendAsync(request);
+            }
         }
         public static Task<HttpResponseMessage> PostStringWithHeaderAsync(this HttpClient httpClient, string requestUri, string contentToSend, string headerName, string headerValue)
         {
-            var content = new StringContent(contentToSend);
-            var request = new HttpRequestMessage(HttpMethod.Post, requestUri) { Content = content };
-            request.Headers.Add(headerName, headerValue);
-            return httpClient.SendAsync(request);
+            using (var content = new StringContent(contentToSend))
+            using (var request = new HttpRequestMessage(HttpMethod.Post, requestUri) { Content = content })
+            {
+                request.Headers.Add(headerName, headerValue);
+                return httpClient.SendAsync(request);
+            }
         }
         public static Task<HttpResponseMessage> PostObjectWithHeaderAsync<T>(this HttpClient httpClient, string requestUri, T contentToSend, string headerName, string headerValue) where T : class
         {
             var serialized = JsonConvert.SerializeObject(contentToSend);
-            var content = new StringContent(serialized);
-            var request = new HttpRequestMessage(HttpMethod.Post, requestUri) { Content = content };
-            request.Headers.Add(headerName, headerValue);
-            return httpClient.SendAsync(request);
+            using (var content = new StringContent(serialized))
+            using (var request = new HttpRequestMessage(HttpMethod.Post, requestUri) { Content = content })
+            {
+                request.Headers.Add(headerName, headerValue);
+                return httpClient.SendAsync(request);
+            }
         }
         public static Task<HttpResponseMessage> DeleteWithHeaderAsync(this HttpClient httpClient, string requestUri, string headerName, string headerValue)
         {
-            var request = new HttpRequestMessage(HttpMethod.Delete, requestUri);
-            request.Headers.Add(headerName, headerValue);
-            return httpClient.SendAsync(request);
+            using (var request = new HttpRequestMessage(HttpMethod.Delete, requestUri))
+            {
+                request.Headers.Add(headerName, headerValue);
+                return httpClient.SendAsync(request);
+            }
         }
     }
 }
