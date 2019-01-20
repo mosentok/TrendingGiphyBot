4b8a9c08ce83a237eaa547ced5f1ff94524b03dc
mosentok <mosentok@gmail.com>
2019-01-19 20:47:16 -0600

    Was afraid of this and yup ulong didn't place nice so changed bot id to a long.

--- TrendingGiphyBot/TrendingGiphyBotFunctions/PostStatsFunction.cs
+++ TrendingGiphyBot/TrendingGiphyBotFunctions/PostStatsFunction.cs
@@ -16,7 +16,7 @@ namespace TrendingGiphyBotFunctions
     {
         static readonly HttpClient _HttpClient = new HttpClient();
         [FunctionName(nameof(PostStatsFunction))]
-        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "postStats/{botid:ulong}")] HttpRequest req, ulong botId, ILogger log)
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "poststats/{botid:long}")] HttpRequest req, long botId, ILogger log)
         {
             log.LogInformation("Posting stats.");
             var statPostsSerialized = Environment.GetEnvironmentVariable("StatPostsSerialized");
@@ -32,7 +32,7 @@ namespace TrendingGiphyBotFunctions
                     if (!response.IsSuccessStatusCode)
                     {
                         var message = await response.Content.ReadAsStringAsync();
-                        log.LogError($"Error posting stats. Status code '{response.StatusCode.ToString()}'. Reason phrase '{response.ReasonPhrase}'. Content '{message}'.");
+                        log.LogError($"Error posting stats. Request uri '{requestUri}'. Status code '{response.StatusCode.ToString()}'. Reason phrase '{response.ReasonPhrase}'. Content '{message}'.");
                     }
                 }
                 catch (Exception ex)
