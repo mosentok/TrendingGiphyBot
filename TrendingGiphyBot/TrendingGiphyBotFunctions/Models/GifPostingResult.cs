b654045d472e335779dde6996208cd0326ee4126
Kirk Horton <mosentok@gmail.com>
2019-01-24 15:43:36 -0600

    Added post gifs function.

---
+++ TrendingGiphyBot/TrendingGiphyBotFunctions/Models/GifPostingResult.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace TrendingGiphyBotFunctions.Models
+{
+    public class GifPostingResult
+    {
+        public List<UrlHistoryContainer> Errors { get; set; }
+        public List<UrlHistoryContainer> ChannelsToDelete { get; set; }
+        public GifPostingResult(List<UrlHistoryContainer> errors, List<UrlHistoryContainer> channelsToDelete)
+        {
+            Errors = errors;
+            ChannelsToDelete = channelsToDelete;
+        }
+    }
+}
---
+++ TrendingGiphyBot/TrendingGiphyBotFunctions/Models/PendingContainer.cs
@@ -0,0 +1,9 @@
+﻿namespace TrendingGiphyBotFunctions.Models
+{
+    public class PendingContainer
+    {
+        public decimal ChannelId { get; set; }
+        public string FirstUnseenUrl { get; set; }
+        public string RandomSearchString { get; set; }
+    }
+}
