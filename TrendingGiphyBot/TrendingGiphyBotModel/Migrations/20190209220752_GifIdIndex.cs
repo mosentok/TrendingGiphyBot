493bba17808ab228b16eff6dfb2e24902201cb2c
mosentok <mosentok@gmail.com>
2019-02-09 16:16:29 -0600

    Added index to UrlHistory.GifId.

---
+++ TrendingGiphyBot/TrendingGiphyBotModel/Migrations/20190209220752_GifIdIndex.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrendingGiphyBotModel.Migrations
+{
+    public partial class GifIdIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_UrlHistory_GifId",
+                table: "UrlHistory",
+                column: "GifId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UrlHistory_GifId",
+                table: "UrlHistory");
+        }
+    }
+}
