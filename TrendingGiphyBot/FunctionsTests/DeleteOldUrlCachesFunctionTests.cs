a315914d243090fb2cf5a552f8af223137aaec5d
Kirk Horton <mosentok@gmail.com>
2019-02-20 15:44:03 -0600

    Added delete url and histories tests.

---
+++ TrendingGiphyBot/FunctionsTests/DeleteOldUrlCachesFunctionTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+using TrendingGiphyBotFunctions;
+using TrendingGiphyBotModel;
+
+namespace FunctionsTests
+{
+    [TestFixture]
+    public class DeleteOldUrlCachesFunctionTests
+    {
+        Mock<ILogger> _Log;
+        Mock<ITrendingGiphyBotContext> _Context;
+        DeleteOldUrlCachesFunction _DeleteOldUrlCachesFunction;
+        [SetUp]
+        public void SetUp()
+        {
+            _Log = new Mock<ILogger>();
+            _Context = new Mock<ITrendingGiphyBotContext>();
+            _DeleteOldUrlCachesFunction = new DeleteOldUrlCachesFunction(_Log.Object, _Context.Object);
+        }
+        [Test]
+        public async Task RunAsync()
+        {
+            var now = DateTime.Now;
+            const int count = 123;
+            _Context.Setup(s => s.DeleteUrlCachesOlderThan(now)).ReturnsAsync(count);
+            var task = _DeleteOldUrlCachesFunction.RunAsync(now);
+            await task;
+            _Context.VerifyAll();
+            Assert.That(task.IsFaulted, Is.False);
+        }
+    }
+}
