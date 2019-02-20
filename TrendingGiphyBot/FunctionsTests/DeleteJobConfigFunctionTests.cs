a65f862ab3c910316cdc22248e66f60833029080
Kirk Horton <mosentok@gmail.com>
2019-02-20 15:19:09 -0600

    Added DeleteJobConfigFunctionTests.

---
+++ TrendingGiphyBot/FunctionsTests/DeleteJobConfigFunctionTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using TrendingGiphyBotFunctions;
+using TrendingGiphyBotModel;
+
+namespace FunctionsTests
+{
+    [TestFixture]
+    public class DeleteJobConfigFunctionTests
+    {
+        Mock<ILogger> _Log;
+        Mock<ITrendingGiphyBotContext> _Context;
+        DeleteJobConfigFunction _DeleteJobConfigFunction;
+        [SetUp]
+        public void SetUp()
+        {
+            _Log = new Mock<ILogger>();
+            _Context = new Mock<ITrendingGiphyBotContext>();
+            _DeleteJobConfigFunction = new DeleteJobConfigFunction(_Log.Object, _Context.Object);
+        }
+        [Test]
+        public async Task RunAsync()
+        {
+            const decimal channelId = 123;
+            var container = new JobConfigContainer();
+            _Context.Setup(s => s.DeleteJobConfig(channelId)).Returns(Task.CompletedTask);
+            var result = await _DeleteJobConfigFunction.RunAsync(channelId);
+            _Context.VerifyAll();
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.InstanceOf<NoContentResult>());
+        }
+    }
+}
