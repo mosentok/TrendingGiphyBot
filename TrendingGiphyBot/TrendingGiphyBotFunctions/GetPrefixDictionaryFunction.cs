da5f691d81f3aea842c8cd22fe863b1e6212f7a6
Kirk Horton <mosentok@gmail.com>
2019-02-20 15:24:50 -0600

    Added GetPrefixDictionaryFunctionTests.

--- TrendingGiphyBot/TrendingGiphyBotFunctions/GetPrefixDictionaryFunction.cs
+++ TrendingGiphyBot/TrendingGiphyBotFunctions/GetPrefixDictionaryFunction.cs
@@ -6,21 +6,30 @@ using Microsoft.Extensions.Logging;
 using System;
 using Microsoft.AspNetCore.Mvc;
 using TrendingGiphyBotModel;
-using System.Collections.Generic;
 
 namespace TrendingGiphyBotFunctions
 {
-    public static class GetPrefixDictionaryFunction
+    public class GetPrefixDictionaryFunction
     {
         [FunctionName(nameof(GetPrefixDictionaryFunction))]
         public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = "prefixDictionary")] HttpRequest req, ILogger log)
         {
-            log.LogInformation("Getting prefix dictionary.");
-            Dictionary<decimal, string> prefixDictionary;
             var connectionString = Environment.GetEnvironmentVariable("TrendingGiphyBotConnectionString");
-            using (var context = new TrendingGiphyBotContext(connectionString))
-                prefixDictionary = await context.GetPrefixDictionary();
-            log.LogInformation($"Got {prefixDictionary.Count} prefixes.");
+            var getPrefixDictionaryFunction = new GetPrefixDictionaryFunction(log, new TrendingGiphyBotContext(connectionString));
+            return await getPrefixDictionaryFunction.RunAsync();
+        }
+        readonly ILogger _Log;
+        readonly ITrendingGiphyBotContext _Context;
+        public GetPrefixDictionaryFunction(ILogger log, ITrendingGiphyBotContext context)
+        {
+            _Log = log;
+            _Context = context;
+        }
+        public async Task<IActionResult> RunAsync()
+        {
+            _Log.LogInformation("Getting prefix dictionary.");
+            var prefixDictionary = await _Context.GetPrefixDictionary();
+            _Log.LogInformation($"Got {prefixDictionary.Count} prefixes.");
             return new OkObjectResult(prefixDictionary);
         }
     }
