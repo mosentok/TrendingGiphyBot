fc0004adc411b80a16ebbb36f8a59cba8470cacd
Kirk Horton <mosentok@gmail.com>
2019-02-20 15:12:28 -0600

    Added GetJobConfigFunctionTests.

---
+++ TrendingGiphyBot/FunctionsTests/GetJobConfigFunctionTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using TrendingGiphyBotFunctions;
+using TrendingGiphyBotModel;
+
+namespace FunctionsTests
+{
+    [TestFixture]
+    public class GetJobConfigFunctionTests
+    {
+        Mock<ILogger> _Log;
+        Mock<ITrendingGiphyBotContext> _Context;
+        GetJobConfigFunction _GetJobConfigFunction;
+        [SetUp]
+        public void SetUp()
+        {
+            _Log = new Mock<ILogger>();
+            _Context = new Mock<ITrendingGiphyBotContext>();
+            _GetJobConfigFunction = new GetJobConfigFunction(_Log.Object, _Context.Object);
+        }
+        [Test]
+        public async Task RunAsync()
+        {
+            const decimal channelId = 123;
+            var container = new JobConfigContainer();
+            _Context.Setup(s => s.GetJobConfig(channelId)).ReturnsAsync(container);
+            var result = await _GetJobConfigFunction.RunAsync(channelId);
+            _Context.VerifyAll();
+            var okObjectResult = result as OkObjectResult;
+            Assert.That(okObjectResult, Is.Not.Null);
+            Assert.That(okObjectResult.Value, Is.EqualTo(container));
+        }
+    }
+}
--- TrendingGiphyBot/TrendingGiphyBotFunctions/GetJobConfigFunction.cs
+++ TrendingGiphyBot/TrendingGiphyBotFunctions/GetJobConfigFunction.cs
@@ -9,17 +9,27 @@ using TrendingGiphyBotModel;
 
 namespace TrendingGiphyBotFunctions
 {
-    public static class GetJobConfigFunction
+    public class GetJobConfigFunction
     {
         [FunctionName(nameof(GetJobConfigFunction))]
         public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = "jobconfigs/{channelid:decimal}")] HttpRequest req, decimal channelId, ILogger log)
         {
-            log.LogInformation($"Channel {channelId} getting job config.");
             var connectionString = Environment.GetEnvironmentVariable("TrendingGiphyBotConnectionString");
-            JobConfigContainer container;
-            using (var context = new TrendingGiphyBotContext(connectionString))
-                container = await context.GetJobConfig(channelId);
-            log.LogInformation($"Channel {channelId} got job config.");
+            var getJobConfigFunction = new GetJobConfigFunction(log, new TrendingGiphyBotContext(connectionString));
+            return await getJobConfigFunction.RunAsync(channelId);
+        }
+        readonly ILogger _Log;
+        readonly ITrendingGiphyBotContext _Context;
+        public GetJobConfigFunction(ILogger log, ITrendingGiphyBotContext context)
+        {
+            _Log = log;
+            _Context = context;
+        }
+        public async Task<IActionResult> RunAsync(decimal channelId)
+        {
+            _Log.LogInformation($"Channel {channelId} getting job config.");
+            var container = await _Context.GetJobConfig(channelId);
+            _Log.LogInformation($"Channel {channelId} got job config.");
             return new OkObjectResult(container);
         }
     }
