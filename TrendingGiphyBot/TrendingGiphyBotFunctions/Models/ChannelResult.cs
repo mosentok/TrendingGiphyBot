cafed68fb92ed9ccce38be9e623431b9b35bffb3
Kirk Horton <mosentok@gmail.com>
2019-02-27 13:52:33 -0600

    Moved the null check on the channel up to where the channel was first retrieved, meaning there's another set of "channels to delete". Changed the channels to delete to lists of decimals.

--- TrendingGiphyBot/TrendingGiphyBotFunctions/Models/ChannelResult.cs
+++ TrendingGiphyBot/TrendingGiphyBotFunctions/Models/ChannelResult.cs
@@ -7,10 +7,12 @@ namespace TrendingGiphyBotFunctions.Models
     {
         public List<ChannelContainer> ChannelContainers { get; set; }
         public List<UrlHistoryContainer> Errors { get; set; }
-        public ChannelResult(List<ChannelContainer> channelContainers, List<UrlHistoryContainer> errors)
+        public List<decimal> ChannelsToDelete { get; set; }
+        public ChannelResult(List<ChannelContainer> channelContainers, List<UrlHistoryContainer> errors, List<decimal> channelsToDelete)
         {
             ChannelContainers = channelContainers;
             Errors = errors;
+            ChannelsToDelete = channelsToDelete;
         }
     }
 }
