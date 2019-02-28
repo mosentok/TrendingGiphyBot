7873e5c2efd095eb1e79644f7eb111a622bf50df
Kirk Horton <mosentok@gmail.com>
2019-02-28 14:12:11 -0600

    Refactored trend helper to not pull dependency values during ctor. Started trend helper tests. Excluded a bunch of tgb core namespaces from coverage.

---
+++ TrendingGiphyBot/TrendingGiphyBotTests/Core/TrendHelperTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Moq;
+using NUnit.Framework;
+using TrendingGiphyBotCore.Helpers;
+
+namespace TrendingGiphyBotTests.Core
+{
+    [TestFixture]
+    public class TrendHelperTests
+    {
+        Mock<IConfiguration> _Config;
+        TrendHelper _TrendHelper;
+        [SetUp]
+        public void SetUp()
+        {
+            _Config = new Mock<IConfiguration>();
+            _TrendHelper = new TrendHelper(_Config.Object);
+        }
+        [TestCase(null, null)]
+        [TestCase("apex legends", "apex legends")]
+        [TestCase("gifs of apex legends", "apex legends")]
+        [TestCase("gif of apex legends", "apex legends")]
+        [TestCase("gifs apex legends", "apex legends")]
+        [TestCase("gif apex legends", "apex legends")]
+        public void CleanRandomSearchString(string randomSearchString, string expectedResult)
+        {
+            var result = _TrendHelper.CleanRandomSearchString(randomSearchString);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
