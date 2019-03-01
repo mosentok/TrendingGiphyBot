6ba3ac91e89a76a876c34ec62012de9a4ac8d6fb
Kirk Horton <mosentok@gmail.com>
2019-03-01 12:27:25 -0600

    Added tests.

--- TrendingGiphyBot/TrendingGiphyBotTests/Core/TrendHelperTests.cs
+++ TrendingGiphyBot/TrendingGiphyBotTests/Core/TrendHelperTests.cs
@@ -1,6 +1,7 @@
 ﻿using Moq;
 using NUnit.Framework;
 using System.Collections.Generic;
+using TrendingGiphyBotCore.Configuration;
 using TrendingGiphyBotCore.Enums;
 using TrendingGiphyBotCore.Helpers;
 using TrendingGiphyBotCore.Wrappers;
@@ -42,11 +43,30 @@ namespace TrendingGiphyBotTests.Core
         [TestCase(Time.Minutes)]
         public void InvalidMinutesConfigMessage(Time time)
         {
-            var validHours = new List<short> { 10, 15, 20, 30 };
-            _Config.Setup(s => s.Get<List<short>>("ValidHours")).Returns(validHours);
+            var validMinutes = new List<short> { 10, 15, 20, 30 };
+            _Config.Setup(s => s.Get<List<short>>("ValidMinutes")).Returns(validMinutes);
             var message = _TrendHelper.InvalidMinutesConfigMessage(time);
-            var expectedMessage = $"When {nameof(Time)} is {time}, interval must be {string.Join(", ", validHours)}.";
+            var expectedMessage = $"When {nameof(Time)} is {time}, interval must be {string.Join(", ", validMinutes)}.";
             Assert.That(message, Is.EqualTo(expectedMessage));
+        }
+        [TestCase(10, Time.Minutes, 24, Time.Hours, "Interval must be between 10 Minutes and 24 Hours.")]
+        [TestCase(123, Time.Minute, 456, Time.Hour, "Interval must be between 123 Minute and 456 Hour.")]
+        public void InvalidConfigRangeMessage(short minInterval, Time minTime, short maxInterval, Time maxTime, string expectedMessage)
+        {
+            var minJobConfig = new SubJobConfig(minInterval, minTime);
+            var maxJobConfig = new SubJobConfig(maxInterval, maxTime);
+            _Config.Setup(s => s.Get<SubJobConfig>("MinJobConfig")).Returns(minJobConfig);
+            _Config.Setup(s => s.Get<SubJobConfig>("MaxJobConfig")).Returns(maxJobConfig);
+            var message = _TrendHelper.InvalidConfigRangeMessage();
+            Assert.That(message, Is.EqualTo(expectedMessage));
+        }
+        [TestCase("Off", true)]
+        [TestCase("asdf", false)]
+        [TestCase(null, false)]
+        public void ShouldTurnCommandOff(string word, bool expectedResult)
+        {
+            var result = _TrendHelper.ShouldTurnCommandOff(word);
+            Assert.That(result, Is.EqualTo(expectedResult));
         }
     }
 }
